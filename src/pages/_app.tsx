5d356d20cc1f4af7b431aa501757fd1f38f502c0
Pedro Val <prmval@gmail.com>
2025-02-11 14:19:18 -0300

    Refactor imports to use consistent quotation marks and enhance component props handling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import React from 'react';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <Component prop1={pageProps.prop1} prop2={pageProps.prop2} />;
 }
